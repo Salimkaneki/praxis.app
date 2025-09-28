26704b96cc796c26a231a162647f9ee820cc9875
Salimkaneki <salimpereira01@gmail.com>
2025-09-28 21:49:43 +0000

    g

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+      '@dashboard': require('path').resolve(__dirname, 'src/app/(features)/dashboard'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
