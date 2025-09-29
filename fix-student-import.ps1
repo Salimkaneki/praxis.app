b13b8473bec5fa05510a4732ab59c9ed9e90b7c1
Salimkaneki <salimpereira01@gmail.com>
2025-09-29 11:14:06 +0000

    chore: Detetion of some files

--- fix-student-import.ps1
+++
@@ -1,5 +0,0 @@
-@'
-$content = Get-Content -Path "C:\Users\_Salim_mevtr_\_sout_\praxis\src\app\(features)\dashboard\student\import\page.tsx" -Raw
-$content = $content -replace 'import ClasseService, \{ Classe \} from "@/app/\(features\)/dashboard/formation/classe/_services/classe.service";', 'import ClasseService, { Classe } from "../../../formation/classe/_services/classe.service";'
-$content | Out-File -FilePath "C:\Users\_Salim_mevtr_\_sout_\praxis\src\app\(features)\dashboard\student\import\page.tsx" -Encoding UTF8
-'@
